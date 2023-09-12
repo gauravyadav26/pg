aaa7befef5ab9b746182bfdc93fe8d892181ffb5
Gaurav Yadav <gauravyadav2601@gmail.com>
2023-09-13 03:14:54 +0530

    a

--- wp-content/plugins/simpletoc/src/accordion.js
+++ wp-content/plugins/simpletoc/src/accordion.js
@@ -1,10 +1,18 @@
-const coll = document.getElementsByClassName( 'simpletoc-collapsible' );
+const buttons = document.querySelectorAll( 'button.simpletoc-collapsible' );
 
-for ( const element of coll ) {
-	element.addEventListener( 'click', function () {
+buttons.forEach( ( button ) => {
+	button.addEventListener( 'click', function () {
 		this.classList.toggle( 'active' );
-		const content = this.nextElementSibling;
+		const content = this.parentElement.nextElementSibling;
 		content.style.display =
 			content.style.display === 'block' ? 'none' : 'block';
+
+		// Toggle aria-expanded attribute on the button
+		const ariaExpanded = this.getAttribute( 'aria-expanded' );
+		if ( ariaExpanded === 'true' ) {
+			this.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			this.setAttribute( 'aria-expanded', 'true' );
+		}
 	} );
-}
+} );
